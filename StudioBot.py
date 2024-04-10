0eeee8bf91b0be8817569593e81bc04790f9f820
sequoia focus <edo.zanella@gmail.com>
2024-04-10 14:53:03 +0200

    arrivato fino al ciclo

--- StudioBot.py
+++ StudioBot.py
@@ -6,7 +6,7 @@
 Simple Bot to keep track of your study and study better
 """
 
-import logging, time, ModuloJson
+import logging, time, ModuloJson, ModuloScelte
 
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update ,ReplyKeyboardRemove
 from telegram.ext import Application, CommandHandler,ConversationHandler, ContextTypes, MessageHandler, filters, CallbackQueryHandler
@@ -15,18 +15,171 @@ import os
 
 load_dotenv()
 bot_token = os.getenv("BOT_TOKEN")
+
+global tempInfo
+global colonne
+
 # Enable logging
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 # set higher logging level for httpx to avoid all GET and POST requests being logged
-logging.getLogger("httpx").setLevel(logging.INFO)
+logging.getLogger("httpx").setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
 
-SCEGLI_TEMPO , SALVA_VOLUME, CHECK_REP= range(3)
+# Stages
+MATERIA, TEMPO_STUDIO, TEMPO_PAUSA, CICLO_FOCUS, STOP , END = range(6)
+# Callback data
+ONE, TWO, THREE, FOUR , FIVE, SIX, SEVEN, EIGHT, NINE, TEN, ELEVEN, TWELVE = range(12)
 
-global tempInfo
+async def start_focus_mode(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Send message on `/start`."""
+    # Get user that sent /start and log his name
+    user = update.message.from_user
+    logger.info("User %s started the conversation.", user.first_name)
+    
+    global tempInfo
+    global colonne
+    tempInfo = {'Materia': "?",
+                'Tempo studiato': 0, 
+                'Tempo_studio': 0,
+                'Tempo_pausa': 0,
+                'Efficenza': 0, 
+                'Note': "?"
+                }
+    colonne = ['Materia', 'Tempo studiato','Tempo_studio','Tempo_pausa', 'Efficenza', 'Note']
+
+    df = ModuloJson.read_csv_table("tabella_studio", colonne)
+    materie_unique = df['Materia'].unique()  # Ottieni tutti i valori unici dalla colonna 'Materia'
+    # Build InlineKeyboard where each button has a displayed text
+    # and a string as callback_data
+    # The keyboard is a list of button rows, where each row is in turn
+    # a list (hence `[[...]]`).
+    keyboard = [
+        [
+            InlineKeyboardButton("Telecomunicazioni", callback_data=str(ONE)),
+            InlineKeyboardButton("Internet and Security", callback_data=str(TWO)),
+        ],
+        [
+            InlineKeyboardButton("Algoritmi per l'Ingegneria", callback_data=str(THREE)),
+            InlineKeyboardButton("Automazione Industriale", callback_data=str(FOUR)),
+        ],
+        [
+            InlineKeyboardButton("Esci", callback_data=str(FIVE)),
+            #InlineKeyboardButton("6", callback_data=str(SIX)),
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    # Send message with text and appended InlineKeyboard
+    await update.message.reply_text("Start handler, Choose a route", reply_markup=reply_markup)
+    # Tell ConversationHandler that we're in state `FIRST` now
+    return MATERIA
+
+async def salva_materia(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Show new choice of buttons"""
+    query = update.callback_query
+    await query.answer()
+    #print(query.data)
+    materia = ModuloScelte.getSubject(int(query.data))
+    #print(materia)
+    global tempInfo
+    tempInfo["Materia"] = materia
+    #print(tempInfo)
+    keyboard = [
+        [
+            InlineKeyboardButton("20m", callback_data=str(ONE)),
+            InlineKeyboardButton("25m", callback_data=str(TWO)),
+            InlineKeyboardButton("30m", callback_data=str(THREE)),
+        ],
+        [
+            InlineKeyboardButton("35m", callback_data=str(FOUR)),
+            InlineKeyboardButton("40m", callback_data=str(FIVE)),
+            InlineKeyboardButton("45m", callback_data=str(SIX)),
+        ],
+        [
+            InlineKeyboardButton("50m", callback_data=str(SEVEN)),
+            InlineKeyboardButton("55m", callback_data=str(EIGHT)),
+            InlineKeyboardButton("60m", callback_data=str(NINE)),
+        ],
+        [
+            InlineKeyboardButton("Esci", callback_data=str(TEN)),
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await query.edit_message_text(
+        text="Bene, ora scegli quanto tempo far durare le tue sessioni di studio", reply_markup=reply_markup
+    )
+    return TEMPO_STUDIO
+
+async def salva_t_studio(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Show new choice of buttons"""
+    query = update.callback_query
+    await query.answer()
+    #print(query.data)
+    studio = ModuloScelte.getStudyTime(int(query.data))
+    #print(materia)
+    global tempInfo
+    tempInfo["Tempo_studio"] = studio
+    #print(tempInfo)
+    keyboard = [
+        [
+            InlineKeyboardButton("5m", callback_data=str(ONE)),
+            InlineKeyboardButton("7m", callback_data=str(TWO)),
+            InlineKeyboardButton("9m", callback_data=str(THREE)),
+        ],
+        [
+            InlineKeyboardButton("10m", callback_data=str(FOUR)),
+            InlineKeyboardButton("12m", callback_data=str(FIVE)),
+            InlineKeyboardButton("14m", callback_data=str(SIX)),
+        ],
+        [
+            InlineKeyboardButton("15m", callback_data=str(SEVEN)),
+            InlineKeyboardButton("17m", callback_data=str(EIGHT)),
+            InlineKeyboardButton("20m", callback_data=str(NINE)),
+        ],
+        [
+            InlineKeyboardButton("Esci", callback_data=str(TEN)),
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await query.edit_message_text(
+        text="Bene, ora scegli quanto tempo far durare le tue pause", reply_markup=reply_markup
+    )
+    return TEMPO_PAUSA
+
+async def salva_t_pausa(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Show new choice of buttons"""
+    query = update.callback_query
+    await query.answer()
+    #print(query.data)
+    pausa = ModuloScelte.getPauseTime(int(query.data))
+    #print(materia)
+    global tempInfo
+    tempInfo["Tempo_pausa"] = pausa
+    print(tempInfo)
+    keyboard = [
+        [
+            InlineKeyboardButton("Cominciamo!", callback_data=str(ONE)),
+        ],
+        [
+            InlineKeyboardButton("Ci ho ripensato", callback_data=str(TWO)),
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await query.edit_message_text(
+        text="Al tuo segnale!", reply_markup=reply_markup
+    )
+    return CICLO_FOCUS
+
+async def end(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Returns `ConversationHandler.END`, which tells the
+    ConversationHandler that the conversation is over.
+    """
+    query = update.callback_query
+    await query.answer()
+    await query.edit_message_text(text="See you next time!")
+    return ConversationHandler.END
 
 # Define a few command handlers. These usually take the two arguments update and context.
 async def inizializza(update: Update, context: ContextTypes.DEFAULT_TYPE)  -> int:
@@ -49,127 +202,6 @@ async def inizializza(update: Update, context: ContextTypes.DEFAULT_TYPE)  -> in
     except Exception as e:
         await update.message.reply_text(e)
 
-
-async def inizioStudio(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    message = '''
-*Diamo inizio al focus
-            🔇🤫
-*'''
-    await update.message.reply_text(message, parse_mode="MarkdownV2")
-    global tempInfo
-
-    colonne = ['Materia', 'Tempo studiato', 'Efficenza', 'Note']
-    df = ModuloJson.read_csv_table("tabella_studio", colonne)
-
-    materie_unique = df['Materia'].unique()  # Ottieni tutti i valori unici dalla colonna 'Materia'
-
-    tempInfo = {'Materia': "?", 'Tempo studiato': "?", 'Efficenza': "?", 'Note': "?"}
-    keyboard = []
-
-    for materia in materie_unique:
-        keyboard.append([InlineKeyboardButton(materia, callback_data=materia)])
-
-    reply_markup = InlineKeyboardMarkup(keyboard)
-
-    await update.message.reply_text("Please choose:", reply_markup=reply_markup)
-    return SCEGLI_TEMPO
-
-async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Parses the CallbackQuery and updates the message text."""
-    query = update.callback_query
-
-    # CallbackQueries need to be answered, even if no notification to the user is needed
-    # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
-    await query.answer()
-
-    await query.edit_message_text(text=f"Selected option: {query.data}")
-
-async def scegliTempo(update: Update, context: ContextTypes.DEFAULT_TYPE)  -> int:
-    text = update.message.text
-    try:
-        global tempInfo
-
-    except:
-        await update.message.reply_text(f"hai cappato duro hermano, riprova", parse_mode="MarkdownV2")
-        return SCEGLI_TEMPO
-
-    return SALVA_VOLUME
-
-
-async def salvaVolume(update: Update, context: ContextTypes.DEFAULT_TYPE)  -> int:
-    try:
-        text = update.message.text
-        amount = int(text)
-        global infoTrade
-        infoTrade["volume"]=amount
-        await update.message.reply_text(f"@{ModuloJson.escape_special_chars(infoTrade['user1'])}, @{ModuloJson.escape_special_chars(infoTrade['user2'])}\nper favore cliccate il seguente comando\n/conferma oppure annullate con /cancel", parse_mode="MarkdownV2")
-    except: 
-        await update.message.reply_text("Inserisci l'importo *in dollari \(us$\)* _\(solo numeri interi\)_ oppure annullate con /cancel", parse_mode="MarkdownV2")
-        return SALVA_VOLUME
-    return CHECK_REP
-
-async def controllaReputazione(update: Update, context: ContextTypes.DEFAULT_TYPE)  -> int:
-    text = update.message.text
-    if text == "/caneclla":
-        await update.message.reply_text("Ciao anon💭", reply_markup=ReplyKeyboardRemove())
-        return ConversationHandler.END
-    global infoTrade
-    username = update.message.from_user.username
-    if (username == infoTrade['user1']):
-        infoTrade['user1Up']= True
-    if (username == infoTrade['user2']):
-        infoTrade['user2Up']= True
-    if (infoTrade['user1Up']==False or infoTrade['user2Up']==False):
-        return CHECK_REP
-    # cerco rep dei due user
-    colonne = ['id', 'username', 'reputazione', 'volume']
-    df = ModuloJson.read_csv_table("reputazioni", colonne)
-    try: 
-        # Seleziona la riga corrispondente al primo utente
-        user1_row = df.loc[df['username'] == infoTrade['user1']]
-        # Se l'utente esiste nel DataFrame
-        if not user1_row.empty:
-            # Ottieni la reputazione dell'utente
-            rep1 = user1_row['reputazione'].values[0]
-            vol1 = user1_row['volume'].values[0]
-        else:
-            await update.message.reply_text(f"Utente 1 {infoTrade['user1']} non trovato, richiedi a @eddichan di essere aggiunto")
-            return ConversationHandler.END
-        # Seleziona la riga corrispondente al primo utente
-        user2_row = df.loc[df['username'] == infoTrade['user2']]
-        # Se l'utente esiste nel DataFrame
-        if not user2_row.empty:
-            # Ottieni la reputazione dell'utente
-            rep2 = user2_row['reputazione'].values[0]
-            vol2 = user2_row['volume'].values[0]
-        else:
-            await update.message.reply_text(f"Utente 2 {infoTrade['user2']} non trovato, richiedi a @eddichan di essere aggiunto")
-            return ConversationHandler.END
-    except Exception as e:
-        # Gestione delle eccezioni
-        await update.message.reply_text(f"Annullo tutto, mi hai rotto il bot!")
-        return ConversationHandler.END
-    
-    if rep1>0 and rep2>0:
-        print("scambio possibile")
-        rep1New = float(rep1+rep2/5)
-        rep2New = float(rep2+rep1/5)
-        df.loc[df['username'] == infoTrade['user1'], 'reputazione'] = rep1New
-        df.loc[df['username'] == infoTrade['user2'], 'reputazione'] = rep2New
-
-        df.loc[df['username'] == infoTrade['user1'], 'volume'] = vol1 + infoTrade['volume']
-        df.loc[df['username'] == infoTrade['user2'], 'volume'] = vol2 + infoTrade['volume']
-        ModuloJson.save_user_data("reputazioni", df)
-        risposta=""
-        risposta=risposta + f"✅La reputazione dell'utente *@{infoTrade['user1']}* è ora *\{int(rep1New)}*\n"
-        risposta=risposta + f"✅La reputazione dell'utente *@{infoTrade['user2']}* è ora *\{int(rep2New)}*\n"
-        await update.message.reply_text(risposta, parse_mode="MarkdownV2")
-
-
-    else:
-        await update.message.reply_text(f"non potete inserire nuove transazioni _senza una reputazione_, contattate @eddichan", parse_mode="MarkdownV2")
-    return ConversationHandler.END
-
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE)  -> int:
     """Send a message when the command /help is issued."""
     help_message = '''
@@ -178,15 +210,6 @@ Possibili comandi:
 /newtrade \- _inserisci un nuovo scambio_
 /help \- _questo messaggio_'''
     await update.message.reply_text(help_message, parse_mode="MarkdownV2")
-
-async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Cancels and ends the conversation."""
-    user = update.message.from_user
-    logger.info("User %s canceled the conversation.", user.first_name)
-    await update.message.reply_text(
-        "Ciao anon💭", reply_markup=ReplyKeyboardRemove()
-    )
-    return ConversationHandler.END
 
 async def stampa(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     colonne = ['username', 'reputazione', 'volume']  # Rimuovi 'id' dalle colonne
@@ -205,31 +228,77 @@ async def stampa(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
         await update.message.reply_text("Il *DataFrame è vuoto* o contiene solo i titoli delle colonne\n\n*Chiedi ad un admin di assegnare le prime reputazioni a persone fidate*", parse_mode="MarkdownV2")
         return
 
-    
+
+
 def main()  -> int:
     """Start the bot."""
     # Create the Application and pass it your bot's token.
     application = Application.builder().token(bot_token).build()
-    
-    tx= ConversationHandler(
-        entry_points=[CommandHandler("s",inizioStudio)],
+
+    # Setup conversation handler with the states FIRST and SECOND
+    # Use the pattern parameter to pass CallbackQueries with specific
+    # data pattern to the corresponding handlers.
+    # ^ means "start of line/string"
+    # $ means "end of line/string"
+    # So ^ABC$ will only allow 'ABC'
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("focus", start_focus_mode)],
         states={
-            SCEGLI_TEMPO: [CommandHandler("cancel", cancel),MessageHandler(filters.ALL, scegliTempo)],
-            SALVA_VOLUME: [CommandHandler("cancel", cancel),MessageHandler(filters.ALL, salvaVolume),],
-            CHECK_REP: [CommandHandler("cancel", cancel),CommandHandler(["conferma","cancella"], controllaReputazione),],
+            MATERIA: [
+                CallbackQueryHandler(salva_materia, pattern="^" + str(ONE) + "$"),
+                CallbackQueryHandler(salva_materia, pattern="^" + str(TWO) + "$"),
+                CallbackQueryHandler(salva_materia, pattern="^" + str(THREE) + "$"),
+                CallbackQueryHandler(salva_materia, pattern="^" + str(FOUR) + "$"),
+                CallbackQueryHandler(end, pattern="^" + str(FIVE) + "$"),
+                #CallbackQueryHandler(four, pattern="^" + str(SIX) + "$"),
+                ],
+            TEMPO_STUDIO: [
+                CallbackQueryHandler(salva_t_studio, pattern="^" + str(ONE) + "$"),
+                CallbackQueryHandler(salva_t_studio, pattern="^" + str(TWO) + "$"),
+                CallbackQueryHandler(salva_t_studio, pattern="^" + str(THREE) + "$"),
+                CallbackQueryHandler(salva_t_studio, pattern="^" + str(FOUR) + "$"),
+                CallbackQueryHandler(salva_t_studio, pattern="^" + str(FIVE) + "$"),
+                CallbackQueryHandler(salva_t_studio, pattern="^" + str(SIX) + "$"),
+                CallbackQueryHandler(salva_t_studio, pattern="^" + str(SEVEN) + "$"),
+                CallbackQueryHandler(salva_t_studio, pattern="^" + str(EIGHT) + "$"),
+                CallbackQueryHandler(salva_t_studio, pattern="^" + str(NINE) + "$"),
+                CallbackQueryHandler(end, pattern="^" + str(TEN) + "$"),
+                ],
+            TEMPO_PAUSA: [
+                CallbackQueryHandler(salva_t_pausa, pattern="^" + str(ONE) + "$"),
+                CallbackQueryHandler(salva_t_pausa, pattern="^" + str(TWO) + "$"),
+                CallbackQueryHandler(salva_t_pausa, pattern="^" + str(THREE) + "$"),
+                CallbackQueryHandler(salva_t_pausa, pattern="^" + str(FOUR) + "$"),
+                CallbackQueryHandler(salva_t_pausa, pattern="^" + str(FIVE) + "$"),
+                CallbackQueryHandler(salva_t_pausa, pattern="^" + str(SIX) + "$"),
+                CallbackQueryHandler(salva_t_pausa, pattern="^" + str(SEVEN) + "$"),
+                CallbackQueryHandler(salva_t_pausa, pattern="^" + str(EIGHT) + "$"),
+                CallbackQueryHandler(salva_t_pausa, pattern="^" + str(NINE) + "$"),
+                CallbackQueryHandler(end, pattern="^" + str(TEN) + "$"),
+                ],
+            CICLO_FOCUS: [
+                #CallbackQueryHandler(one, pattern="^" + str(ONE) + "$"),
+                CallbackQueryHandler(end, pattern="^" + str(TWO) + "$"),
+                ],
+            STOP: [
+                #CallbackQueryHandler(one, pattern="^" + str(ONE) + "$"),
+                CallbackQueryHandler(end, pattern="^" + str(TWO) + "$"),
+                ],
+            END: [
+                CallbackQueryHandler(end, pattern="^" + str(ONE) + "$"),
+                CallbackQueryHandler(start_focus_mode, pattern="^" + str(TWO) + "$"),
+                ],
+
         },
-        fallbacks=[CommandHandler("cancel", cancel),],
-        per_user=False
+        fallbacks=[CommandHandler("start", start_focus_mode)],
     )
-    # Add the conversation handler to the application
-    application.add_handler(tx)    
-    # on different commands - answer in Telegram
-    application.add_handler(CommandHandler("start", inizializza))
-    application.add_handler(CallbackQueryHandler(button))
+
+    # Add ConversationHandler to application that will be used for handling updates
+    application.add_handler(conv_handler)
 
     # application.add_handler(CommandHandler("manualRep", manualRep))
     application.add_handler(CommandHandler("help", help_command))
-    application.add_handler(CommandHandler("reps", stampa))
+    application.add_handler(CommandHandler("print", stampa))
 
     # Run the bot until the user presses Ctrl-C
     application.run_polling(allowed_updates=Update.ALL_TYPES)
